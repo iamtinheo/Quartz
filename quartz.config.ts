403e120b33060c5f2551d6c0becc437bfec8ccc7
khang <hongvup@gmail.com>
2026-02-03 12:50:26 +0700

    Quartz sync: Feb 3, 2026, 12:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "iamtinheo.github.io/Quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
